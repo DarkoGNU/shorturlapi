eb017680aa6413fa32fb29e55f0b55e4c72bc2a5
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2024-03-27 23:09:52 +0100

    PostgreSQL working

--- src/test/java/com/example/shorturlapi/ShorturlapiApplicationTests.java
+++ src/test/java/com/example/shorturlapi/ShorturlapiApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.shorturlapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ShorturlapiApplicationTests {
 
 	@Test
