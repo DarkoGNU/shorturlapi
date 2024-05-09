7b68ba1a50d090b0dcc9a3505d5eacab48ab7684
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2024-05-09 14:19:09 +0200

    Kafka queue works

--- shortener/src/main/java/com/example/shortener/KafkaProducerConfig.java
+++ shortener/src/main/java/com/example/shortener/KafkaProducerConfig.java
@@ -1,13 +1,13 @@
 package com.example.shortener;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.List;
@@ -27,10 +27,10 @@ public class KafkaProducerConfig {
                 bootstrapAddress);
         configProps.put(
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
+                JsonSerializer.class);
         configProps.put(
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
+                JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
